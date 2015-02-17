d5863b13c624188f3efa8c69ec7bbbc2cf969061
Gustavo Olivares <gustavo@niwa-30587.niwa.local>
2015-02-18 12:40:02 +1300

    Reading from the port

--- logger_main.py
+++ logger_main.py
@@ -1,15 +1,26 @@
 # Load the library
 import serial
+# Hacks to work with custom end of line
+eol = b'\r'
+leneol = len(eol)
+line = bytearray()
 # Open the serial port and clean the I/O buffer
-ser = serial.Serial('/dev/ttyUSB0',11500)
+ser = serial.Serial('/dev/ttyUSB0',115200)
 ser.flushInput()
 ser.flushOutput()
 # Start the logging
 while True:
 	# Get a line of data from the instrument
-	current_line = ser.readLine(None, '/n')
+	while True:
+        c = ser.read(1)
+        if c:
+            line += c
+            if line[-leneol:] == eol:
+                break
+        else:
+            break
 	# Debugging only ... REMOVE IT FOR DEPLOYMENT
-	print(current_line)
+	print(line)
 	# Parse the data line
 	
 	# Add timestamp
@@ -27,3 +38,4 @@ while True:
 	# Is it the last minute of the day?
 	# YES! compress previous day's file
 	
+print('I\'m done')
