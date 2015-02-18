f196ddf00b0f129a179029182ec95ea9cb383594
Gustavo Olivares <gustavo@niwa-30587.niwa.local>
2015-02-18 14:14:06 +1300

    Now the serial port is behaving. The line is divided in data and error. The records have a timestamp with them.
    TODO: Figure out which file to open to save
    
     Committer: Gustavo Olivares <gustavo@niwa-30587.niwa.local>
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    	modified:   logger_main.py

--- logger_main.py
+++ logger_main.py
@@ -5,12 +5,18 @@ eol = b'\r'
 leneol = len(eol)
 line = bytearray()
 # Open the serial port and clean the I/O buffer
-ser = serial.Serial('/dev/ttyUSB1',9600)
+ser = serial.Serial('/dev/ttyUSB0',115200)
 ser.flushInput()
 ser.flushOutput()
+# Set the time constants
+rec_time=time.gmtime()
+timestamp = time.strftime("%Y/%m/%d %H:%M:%S GMT",rec_time)
+prev_minute=rec_time[4]
 # Start the logging
 while True:
-	# Get a line of data from the instrument
+	# Request a data line from the instrument
+	ser.write('rall')
+	# Get the line of data from the instrument
 	while True:
 		c = ser.read(1)
 		line += c
@@ -19,14 +25,20 @@ while True:
 	# Debugging only ... REMOVE IT FOR DEPLOYMENT
 	print(line)
 	# Parse the data line
-	
-	# Add timestamp
-	
+	# Set the time for the record
+	rec_time=time.gmtime()
+	timestamp = time.strftime("%Y/%m/%d %H:%M:%S GMT",rec_time)
+	# SAMPLE LINE ONLY
+	line = '3.15E+01,0000,39.0,14.0,40.0,33.0,98.8,54.9,0.054,31,FULL (2475)'
+	split_indx=line.find(',')
+	concentration = eval(line[:split_indx])
+	error_message = line[split_indx+1:]
 	# Make the line pretty for the file
-	
+	file_line = timestamp+','+line
 	# Save it to the appropriate file
 	
 	# Is it the top of the minute?
+	if rec_time[4] == prev_minute:
 		# YES! --> generate the psql statement
 		
 		# Update the database
