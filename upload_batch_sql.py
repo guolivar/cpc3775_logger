6dff7ffa8c964bd37b4742702ba177ebbabd713e
Gustavo Olivares <guolivar@gmail.com>
2015-03-06 10:22:50 +1300

    File name issues and more verbose SQL uploader
    Signed-off-by: Gustavo Olivares <guolivar@gmail.com>

--- upload_batch_sql.py
+++ upload_batch_sql.py
@@ -10,12 +10,15 @@ port = settings_file.readline().rstrip('\n')
 # path for data files
 # e.g. "/home/logger/datacpc3775/"
 datapath = settings_file.readline().rstrip('\n') + "SQL/"
+print(datapath)
 # psql connection string
 # e.g "user=datauser password=l3tme1n host=penap-data.dyndns.org dbname=didactic port=5432"
 db_conn = settings_file.readline().rstrip('\n')
+print(db_conn)
 # ID values for the parameters and site (DATA, ERROR, SITE)
 # e.g. "408,409,2" == CPCdata,CPCerror,QueenStreet
 params = settings_file.readline().rstrip('\n').split(",")
+print(params)
 # Close the settings file
 settings_file.close()
 # Set the time constants
@@ -25,13 +28,17 @@ try:
 except psycopg2.Error, e:
 	pass
 else:
-	subprocess.call(["mv",datapath + "missed_inserts.sql",datapath + "processing.sql"])
+	subprocess.call(["mv",datapath + "inserts.sql",datapath + "processing.sql"])
+	print("Moved the inserts file")
 	sql_buffer = open(datapath + "processing.sql","r")
+	print("Read the processing sql file")
 	cur = con.cursor()
 	for line in sql_buffer:
+		print(line)
 		cur.execute(line)
 	con.commit()
 	cur.close()
 	con.close()
 	subprocess.call(["mv",datapath + "processing.sql",datapath + prefix + "processing.sql"])
 	subprocess.call(["gzip",datapath + prefix + "processing.sql"])
+print("I'm done")
